2fd5bf1c0d4734a1c4be0cc69b7d0f863f5cda4b
Nanny <132921996+nannywlkk@users.noreply.github.com>
2025-04-15 12:33:59 -0300

    c

--- kivy.py
+++ kivy.py
@@ -1,28 +1,46 @@
+from kivymd.uix.screen import Screen
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.screen import Screen
 from kivymd.uix.toolbar import MDTopAppBar
 
 class ContadorApp(MDApp):
     def build(self):
         self.contador = 0
 
-        screen = Screen()
-        main_layout = MDBoxLayout(orientation="vertical", padding=20, spacing=20)
+        layout = MDBoxLayout(orientation="vertical", spacing=20, padding=20)
 
         toolbar = MDTopAppBar(title="Contador KivyMD")
-        toolbar.pos_hint = {"top": 1}
-        main_layout.add_widget(toolbar)  
+        layout.add_widget(toolbar)
+
 
         self.label = MDLabel(
-            text=str(self.contador),
+            int(self.contador),    
             halign="center",
             font_style="H3"
         )
-        main_layout.add_widget(self.label)
+        layout.add_widget(self.label)
 
-        btn_layout = MDBoxLayout(spacing=20, padding=10, size_hint=(1, None), height=50)
-        btn_menos = MDRaisedButton(text="-", on_release=self.decrementar)
+
+        btn_layout = MDBoxLayout(spacing=20)
         btn_mas = MDRaisedButton(text="+", on_release=self.incrementar)
+        btn_menos = MDRaisedButton(text="-", on_release=self.reducir)
+        btn_layout.add_widget(btn_menos)
+        btn_layout.add_widget(btn_mas)
+
+        layout.add_widget(btn_layout)
+
+        screen = Screen()
+        screen.add_widget(layout)
+        return screen
+
+    def incrementar(self, instance):
+        self.contador += 1
+        self.label.text = str(self.contador)
+
+    def reducir(self, instance):
+        self.contador -= 1
+        self.label.text = str(self.contador)
+
+ContadorApp().run()
